c35ef344518ef6c8cfbf2fcf06ab1552d134aac9
michaelprince3 <michaelprince3@gmail.com>
2020-04-21 12:04:20 -0500

    dictionary

---
+++ dictionaryOfWords.py
@@ -0,0 +1,11 @@
+word_definitions = dict()
+
+word_definitions["Awesome"] = "The feeling of students when they are learning Python"
+word_definitions["Amazing"] = "The feeling you get when you write good Python"
+word_definitions["Boolean"] = True
+
+
+for (key, value) in word_definitions.items():
+    print(f'The definition of {key} is {value}')
+
+    
